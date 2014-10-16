108813cd7a2ce5db4c2052f44d21f75a80737545
R-Rajkumar <rraju1990@gmail.com>
2014-10-16 15:10:41 +0530

    Update variables.php

--- variables/variables.php
+++ variables/variables.php
@@ -1,6 +1,6 @@
 <?php
 
-$heading='Apache Stratos 4.1.0 M2 Developer Preview';
+$heading='Apache Stratos 4.1.0 M2 Developer Preview.';
 
 $footer='Copyright &copy; 2010 1stWebDesigner PHP Template';
 
