ef962e9803a64c2629d12b518f6d4bc5c4791d05
R-Rajkumar <rraju1990@gmail.com>
2014-10-16 15:09:03 +0530

    i

--- index.php
+++ index.php
@@ -28,19 +28,13 @@
 
 <div id="content">
 
-<h1>Heading1</h1>
-<h2>Heading2</h2>
-<h3>Heading3</h3>
-<h4>Heading4</h4>
-<h5>Heading5</h5>
+<h1>Apache Stratos</h1>
+<h2>Kubernetes</h2>
+<h3>CoreOS</h3>
+<h4>Docker</h4>
+<h5>#1</h5>
 
-<h3>Paragraph Element</h3>
-
-<p>
-
-Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-
-</p>
+<h3>Apache Stratos</h3>
 
 <p>
 
@@ -48,7 +42,7 @@ Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non u
 
 </p>
 
-<h3>Another Heading Starting Point</h3>
+<h3>Kubernetes</h3>
 
 <p>
 
@@ -56,11 +50,22 @@ Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non u
 
 </p>
 
+<h3>CoreOS</h3>
+
 <p>
 
 Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
 
 </p>
+
+<h3>Docker</h3>
+
+<p>
+
+Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+
+</p>
+
 
 </div> <!-- end #content -->
 
